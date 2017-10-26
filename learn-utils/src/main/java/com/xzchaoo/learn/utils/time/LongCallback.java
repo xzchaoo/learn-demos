19f9b8ad217846f2e6bc7e7a732067f8eb20bbbe
zcxu <zcxu@Ctrip.com>
2017-10-26 21:41:49 +0800

    新增utils模块

---
+++ learn-utils/src/main/java/com/xzchaoo/learn/utils/time/LongCallback.java
@@ -0,0 +1,11 @@
+package com.xzchaoo.learn.utils.time;
+
+/**
+ * created by zcxu at 2017/10/26
+ *
+ * @author zcxu
+ */
+@FunctionalInterface
+public interface LongCallback {
+    void callback(long time);
+}
