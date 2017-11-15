31c18c826f2084cf87325770b7071376514b3bb5
zcxu <zcxu@Ctrip.com>
2017-11-15 17:03:15 +0800

    禁用掉一些测试

--- learn-other/learn-compress/src/test/java/com/xzchaoo/learn/other/compress/AvroTest.java
+++ learn-other/learn-compress/src/test/java/com/xzchaoo/learn/other/compress/AvroTest.java
@@ -1,25 +1,25 @@
-package com.xzchaoo.learn.other.compress;
-
-import org.apache.avro.file.DataFileWriter;
-import org.apache.avro.io.DatumWriter;
-import org.apache.avro.specific.SpecificDatumWriter;
-import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import example.avro.User;
-
-public class AvroTest {
-	@Test
-	public void test() throws IOException {
-		User u = User.newBuilder().setId(1).setName("xzc").setLastLoginAt(System.currentTimeMillis()).build();
-		DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
-		DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		dataFileWriter.create(u.getSchema(), baos);
-		dataFileWriter.append(u);
-		dataFileWriter.close();
-		System.out.println(baos.size());
-	}
-}
+//package com.xzchaoo.learn.other.compress;
+//
+//import org.apache.avro.file.DataFileWriter;
+//import org.apache.avro.io.DatumWriter;
+//import org.apache.avro.specific.SpecificDatumWriter;
+//import org.junit.Test;
+//
+//import java.io.ByteArrayOutputStream;
+//import java.io.IOException;
+//
+//import example.avro.User;
+//
+//public class AvroTest {
+//	@Test
+//	public void test() throws IOException {
+//		User u = User.newBuilder().setId(1).setName("xzc").setLastLoginAt(System.currentTimeMillis()).build();
+//		DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+//		DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//		dataFileWriter.create(u.getSchema(), baos);
+//		dataFileWriter.append(u);
+//		dataFileWriter.close();
+//		System.out.println(baos.size());
+//	}
+//}
---
+++ learn-springboot/src/test/java/com/ctrip/UTTest.java
@@ -0,0 +1,19 @@
+package com.ctrip;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * created by zcxu at 2017/11/15
+ *
+ * @author zcxu
+ */
+public class UTTest {
+
+  @Test
+  public void test1() {
+    assertTrue(true);
+  }
+
+}
