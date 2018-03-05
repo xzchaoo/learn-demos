0b535e372f3b366d0a5c2cc1c101a635b10870ea
zcxu <zcxu@Ctrip.com>
2018-03-05 13:20:42 +0800

    完善eclipse-collections例子

--- learn-other/learn-eclipse-collections/src/test/java/com/xzchaoo/learn/eclipse/collections/SetTest.java
+++ learn-other/learn-eclipse-collections/src/test/java/com/xzchaoo/learn/eclipse/collections/SetTest.java
@@ -1,8 +1,11 @@
 package com.xzchaoo.learn.eclipse.collections;
 
+import org.eclipse.collections.api.set.FixedSizeSet;
 import org.eclipse.collections.api.set.ImmutableSet;
 import org.eclipse.collections.impl.factory.Sets;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * created by xzchaoo at 2017/11/25
@@ -10,9 +13,20 @@ import org.junit.Test;
  * @author xzchaoo
  */
 public class SetTest {
-	@Test
-	public void test() {
-		ImmutableSet<Integer> s = Sets.immutable.of(1, 2, 3);
-		s.castToSet();
-	}
+  @Test
+  public void test_singleton() {
+    //大小fixed不可改变 因此不能删除和添加
+    FixedSizeSet<Integer> set = Sets.fixedSize.with(1);
+    assertThat(set.size()).isEqualTo(1);
+
+    //这两个有什么区别
+    ImmutableSet<Integer> set2 = Sets.immutable.of(1);
+    assertThat(set2.size()).isEqualTo(1);
+  }
+
+  @Test
+  public void test() {
+    ImmutableSet<Integer> s = Sets.immutable.of(1, 2, 3);
+    s.castToSet();
+  }
 }
