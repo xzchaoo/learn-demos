48bc78e728cb1d74c223fe83babebd4e52d05836
xzchaoo <70862045@qq.com>
2018-06-21 20:02:17 +0800

    完善代码

--- utils/guava/src/test/java/com/xzchaoo/learn/guava/CacheTest.java
+++ utils/guava/src/test/java/com/xzchaoo/learn/guava/CacheTest.java
@@ -22,88 +22,99 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.*;
 
 public class CacheTest {
-	@Test
-	public void caceh_1() throws Exception {
-		Cache<Integer, Integer> c = CacheBuilder.newBuilder()
-			.maximumSize(2)
-			.build();
-		c.put(1, 1);
-		assertEquals(1, c.size());
-		c.put(2, 2);
-		assertEquals(2, c.size());
-		c.put(3, 3);
-		assertEquals(2, c.size());
-	}
+  @Test
+  public void test_weakKeys() throws Exception {
+    Cache<Integer, Integer> c = CacheBuilder.newBuilder()
+      // 弱key会被移除
+      .weakKeys()
+      .build();
+    c.put(1, 1);
+    assertEquals(1, c.size());
+    c.put(2, 2);
+    assertEquals(2, c.size());
+    c.put(3, 3);
+    assertEquals(3, c.size());
+    c.put(1111, 2222);
+    System.gc();
+    Thread.sleep(1000);
+    c.cleanUp();
+    System.out.println(c.size());
+  }
 
-	@Test
-	public void caceh_2() throws Exception {
-		Cache<Integer, Integer> c = CacheBuilder.newBuilder()
-			.maximumWeight(3)
-			.recordStats()
-			.weigher((Weigher<Integer, Integer>) (key, value) -> key)
-			.build();
-		c.put(1, 1);
-		assertEquals(1, c.size());
-		c.put(2, 2);
-		assertEquals(2, c.size());
-		c.put(3, 3);
-		assertEquals(1, c.size());
-		//4这个元素根本放不进去 因为权重超过总权重
-		c.put(4, 4);
-		assertEquals(1, c.size());
+  @Test
+  public void lazyInitMap() throws Exception {
+    //一个懒初始化的缓存
+    LoadingCache<Integer, Integer> lc = CacheBuilder.newBuilder()
+      .expireAfterAccess(100, TimeUnit.MILLISECONDS)
+      .build(new CacheLoader<Integer, Integer>() {
+        @Override
+        public Integer load(Integer key) throws Exception {
+          System.out.println("init " + key);
+          Thread.sleep(1000);
+          return key;
+        }
+      });
+    new Thread(() -> {
+      try {
+        System.out.println(lc.get(1));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }).start();
+    new Thread(() -> {
+      try {
+        System.out.println(lc.get(1));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }).start();
+    Thread.sleep(2000);
+    System.out.println(lc.getIfPresent(1));
+    System.out.println(lc.getUnchecked(1));
+    // 将缓存转成并发Map
+    // lc.asMap();
+    System.out.println(lc.getIfPresent(4));
+  }
 
-		CacheStats cs = c.stats();
-		System.out.println(cs);
-	}
+  @Test
+  public void caceh_3() throws Exception {
+    Cache<Integer, Integer> c = CacheBuilder.newBuilder()
+      .concurrencyLevel(1)
+      .expireAfterWrite(100, TimeUnit.MILLISECONDS)
+      .recordStats()
+      .build();
+    c.put(1, 1);
+    assertEquals(1, c.size());
+    assertEquals(1, c.getIfPresent(1).intValue());
+    Thread.sleep(200);
+    //需要手动清理 cache背后并没有线程自动清理
+    c.cleanUp();
+    assertEquals(0, c.size());
+  }
 
-	@Test
-	public void caceh_3() throws Exception {
-		Cache<Integer, Integer> c = CacheBuilder.newBuilder()
-			.concurrencyLevel(1)
-			.expireAfterWrite(100, TimeUnit.MILLISECONDS)
-			.recordStats()
-			.build();
-		c.put(1, 1);
-		assertEquals(1, c.size());
-		assertEquals(1, c.getIfPresent(1).intValue());
-		Thread.sleep(200);
-		//需要手动清理 cache背后并没有线程自动清理
-		c.cleanUp();
-		assertEquals(0, c.size());
-	}
+  /**
+   * 权重版 这个应该很少用
+   *
+   * @throws Exception
+   */
+  @Test
+  public void test_weight() throws Exception {
+    Cache<Integer, Integer> c = CacheBuilder.newBuilder()
+      .maximumWeight(3)
+      .recordStats()
+      .weigher((Weigher<Integer, Integer>) (key, value) -> key)
+      .build();
+    c.put(1, 1);
+    assertEquals(1, c.size());
+    c.put(2, 2);
+    assertEquals(2, c.size());
+    c.put(3, 3);
+    assertEquals(1, c.size());
+    //4这个元素根本放不进去 因为权重超过总权重
+    c.put(4, 4);
+    assertEquals(1, c.size());
 
-	@Test
-	public void lazyInitMap() throws Exception {
-		//一个懒初始化的缓存
-		LoadingCache<Integer, Integer> lc = CacheBuilder.newBuilder()
-			.expireAfterAccess(100, TimeUnit.MILLISECONDS)
-			.build(new CacheLoader<Integer, Integer>() {
-				@Override
-				public Integer load(Integer key) throws Exception {
-					System.out.println("init " + key);
-					Thread.sleep(1000);
-					return key;
-				}
-			});
-		new Thread(() -> {
-			try {
-				System.out.println(lc.get(1));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}).start();
-		new Thread(() -> {
-			try {
-				System.out.println(lc.get(1));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}).start();
-		Thread.sleep(2000);
-		System.out.println(lc.getIfPresent(1));
-		System.out.println(lc.getUnchecked(1));
-		//将缓存转成并发Map
-		lc.asMap();
-		System.out.println(lc.getIfPresent(4));
-	}
+    CacheStats cs = c.stats();
+    System.out.println(cs);
+  }
 }
--- utils/guava/src/test/java/com/xzchaoo/learn/guava/FutureTest.java
+++ utils/guava/src/test/java/com/xzchaoo/learn/guava/FutureTest.java
@@ -1,5 +1,6 @@
 package com.xzchaoo.learn.guava;
 
+import com.google.common.util.concurrent.FluentFuture;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -9,6 +10,7 @@ import com.google.common.util.concurrent.SettableFuture;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -16,69 +18,103 @@ import java.util.concurrent.TimeUnit;
 import javax.annotation.Nullable;
 
 public class FutureTest {
-	@Test
-	public void test_SettableFuture() {
-		SettableFuture<String> sf = SettableFuture.create();
-		sf.addCallback(new FutureCallback<String>() {
-			@Override
-			public void onSuccess(@Nullable String result) {
-				System.out.println(result);
-			}
+  @Test
+  public void test_SettableFuture() throws InterruptedException {
+    // sf暴露了一些方法 可以让调用方控制该future何时完成或失败
+    SettableFuture<String> sf = SettableFuture.create();
+    CountDownLatch cdl = new CountDownLatch(1);
+    sf.addCallback(new FutureCallback<String>() {
+      @Override
+      public void onSuccess(@Nullable String result) {
+        System.out.println(result);
+        cdl.countDown();
+      }
 
-			@Override
-			public void onFailure(Throwable t) {
+      @Override
+      public void onFailure(Throwable t) {
 
-			}
-		}, MoreExecutors.directExecutor());
-		sf.set("a");
-	}
+      }
+    }, MoreExecutors.directExecutor());
+    sf.set("a");
+    cdl.await();
+  }
 
-	@Test
-	public void test_allAsList() {
-		SettableFuture<Integer> sf1 = SettableFuture.create();
-		SettableFuture<Integer> sf2 = SettableFuture.create();
-		ListenableFuture<List<Integer>> allFuture = Futures.allAsList(sf1, sf2);
-		allFuture.addListener(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					System.out.println(allFuture.get());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}, MoreExecutors.directExecutor());
-		sf1.set(1);
-		sf2.set(2);
-	}
+  @Test
+  public void test_allAsList() {
+    // 全部成功才算成功
+    SettableFuture<Integer> sf1 = SettableFuture.create();
+    SettableFuture<Integer> sf2 = SettableFuture.create();
+    ListenableFuture<List<Integer>> allFuture = Futures.allAsList(sf1, sf2);
+    allFuture.addListener(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          System.out.println(allFuture.get());
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    }, MoreExecutors.directExecutor());
+    sf1.set(1);
+    sf2.set(2);
+  }
 
-	@Test
-	public void test_timeout() throws InterruptedException {
-		ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+  @Test
+  public void test_timeout() throws InterruptedException {
+    ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
 
-		try {
-			//sf是原始的F
-			SettableFuture<Integer> sf = SettableFuture.create();
+    try {
+      //sf是原始的F
+      SettableFuture<Integer> sf = SettableFuture.create();
 
-			//具备timeout能力的F
-			ListenableFuture<Integer> timeoutF = Futures.withTimeout(sf, 1, TimeUnit.SECONDS, ses);
+      //具备timeout能力的F
+      ListenableFuture<Integer> timeoutF = Futures.withTimeout(sf, 1, TimeUnit.SECONDS, ses);
 
-			Futures.addCallback(timeoutF, new FutureCallback<Integer>() {
-				@Override
-				public void onSuccess(@Nullable Integer result) {
-					System.out.println(result);
-				}
+      Futures.addCallback(timeoutF, new FutureCallback<Integer>() {
+        @Override
+        public void onSuccess(@Nullable Integer result) {
+          System.out.println(result);
+        }
 
-				@Override
-				public void onFailure(Throwable t) {
-					t.printStackTrace();
-				}
-			}, MoreExecutors.directExecutor());
+        @Override
+        public void onFailure(Throwable t) {
+          t.printStackTrace();
+        }
+      }, MoreExecutors.directExecutor());
 
-			//强制睡觉1.5秒 上述onFailure会收到超时
-			Thread.sleep(1500);
-		} finally {
-			ses.shutdownNow();
-		}
-	}
+      //强制睡觉1.5秒 上述onFailure会收到超时
+      Thread.sleep(1500);
+    } finally {
+      ses.shutdownNow();
+    }
+  }
+
+  /**
+   * guava23之后新增了流式API, 可以更流畅地进行编程
+   * 功能并没有增强, Futures 本来已经提供了这些功能
+   *
+   * @throws Exception
+   */
+  @Test
+  public void test_fluent() throws Exception {
+    final ListenableFuture<String> f1 = Futures.immediateFuture("123");
+    final FluentFuture<String> f2 = FluentFuture.from(f1)
+      .transform(Integer::parseInt, MoreExecutors.directExecutor())
+      .transform(x -> x * x, MoreExecutors.directExecutor())
+      .transformAsync(x -> {
+        final SettableFuture<String> sf = SettableFuture.create();
+        new Thread(() -> {
+          try {
+            Thread.sleep(3000);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          sf.set("i am result");
+        }).start();
+        return sf;
+      }, MoreExecutors.directExecutor());
+
+    final String result = f2.get();
+    System.out.println(result);
+  }
 }
